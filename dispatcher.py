fe996625cab38d83f31f68cc1e9b443eba408022
wuyinengyan <liuhengjuncn@sina.com>
2019-12-04 16:15:46 +0800

    2. 添加Dispatcher.py

--- dispatcher.py
+++ dispatcher.py
@@ -1,6 +1,18 @@
 class Dispatcher:
-    def reg(self):
+    def reg(self, cmd, fn):
         pass
 
     def run(self):
-        pass
+        while True:
+            cmd = input(">>>").strip()
+            if cmd == "quit":
+                break
+            getattr(self, cmd, self.defaultfn)()
+
+    def defaultfn(self):
+        print("Unknown command")
+
+
+if __name__ == '__main__':
+    d = Dispatcher()
+    d.run()
